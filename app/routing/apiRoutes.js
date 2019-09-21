cf9f83b14bc8ef9a3f39b1112eb97344a411eb22
Stephen Thompson <smt3@hotmail.com>
2019-09-20 23:55:02 -0500

    update

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,15 @@
+var path = require('path');
+var friends = require('../data/friends');
+module.exports = function(app) {
+
+    app.get('/api/friends', function(req, res) {
+      return res.json(friends);
+    });
+  
+    app.post('/api/friends', function(req, res) {
+      
+        for(i=0; i < friends.length; i++){
+            
+        }
+    });
+  };
