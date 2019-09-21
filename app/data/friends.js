abe8c2aa014364b7d3e6d98e3f12c437cd5d3521
Stephen Thompson <smt3@hotmail.com>
2019-09-20 23:54:42 -0500

    update

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friends =[
+    {
+        'name': 'Stephen',
+        'photo': 'https://avatars3.githubusercontent.com/u/51222391?s=460&v=4',
+        'scores': ['5','1','4','4','5','1','2','5','4','1']
+    }
+];
+
+module.exports = friends;
